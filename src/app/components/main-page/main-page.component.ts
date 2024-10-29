1e1df8d688a57a3a896403ea4bc32226aeba7ef4
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-10-29 16:09:08 +0100

    changes

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GetStartedComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
